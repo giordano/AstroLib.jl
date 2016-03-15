4b9e9ec83d4c9ef1b502825e97d99520c9676a09
Mosè Giordano <mose@gnu.org>
2016-03-15 13:13:31 +0100

    No accented characters in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -51,8 +51,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'AstroLib.jl'
-copyright = '2016, Mosè Giordano'
-author = 'Mosè Giordano'
+author = 'Mose\' Giordano'
+copyright = '2016, ' + author
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -227,7 +227,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'AstroLibjl.tex', 'AstroLib.jl Documentation',
-     'Mosè Giordano', 'manual'),
+     author, 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
