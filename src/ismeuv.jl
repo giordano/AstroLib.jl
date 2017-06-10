a960e08dc857c652643d27cbde09d1ae056cbab7
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2017-06-10 23:42:18 +0530

    Performance and type improvements

--- src/ismeuv.jl
+++ src/ismeuv.jl
@@ -3,19 +3,19 @@
 function _ismeuv{T<:AbstractFloat}(wave::T, hcol::T, he1col::T, he2col::T, fano::Bool)
     r = wave/911.75
     if r > 1
-        return T(0)
+        return zero(T)
     end
     # minexp = -708.39642  from alog((machar(double=double).xmin)) where double = 1b in IDL
-    z = sqrt(r/(1.0 - r))
-    denom = -expm1(-2*π*z)
-    tauH = (hcol*(3.44e-16)*(r^4)*exp(-4.0*z*atan(1/z)))/denom
+    z = sqrt(r/(1 - r))
+    denom = -expm1(-2*z*π)
+    tauH = ((3.44e-16)*(r^4)*exp(-4*z*atan(1/z))*hcol)/denom
     r *= 4
     tauHe2 = zero(T)
     if r < 1
-        z = sqrt(r/(1.0 - r))
+        z = sqrt(r/(1 - r))
         denom = T(4)
-        denom *= -expm1(-2*π*z)
-        tauHe2 = (he2col*(3.44e-16)*(r^4)*exp(-4.0*z*atan(1/z)))/denom
+        denom *= -expm1(-2*z*π)
+        tauHe2 = ((3.44e-16)*(r^4)*exp(-4*z*atan(1/z))*he2col)/denom
     end
     q  = [2.81, 2.51, 2.45, 2.44]
     nu = [1.610, 2.795, 3.817, 4.824]
@@ -39,7 +39,7 @@ function _ismeuv{T<:AbstractFloat}(wave::T, hcol::T, he1col::T, he2col::T, fano:
                 end
             end
         end
-    tauHe1 = he1col*exp10(y)
+    tauHe1 = exp10(y)*he1col
     end
     return tauH + tauHe1 + tauHe2
 end
@@ -92,7 +92,7 @@ julia> ismeuv.([670, 910], 1e19, 5e17, 5e17)
 
 Code of this function is based on IDL Astronomy User's Library.
 """
-ismeuv(wave::Real, hcol::Real, he1col::Real=hcol*0.1, he2col::Real=0,
+ismeuv(wave::Real, hcol::Real, he1col::Real=hcol/10, he2col::Real=0,
        fano::Bool=false) =
            _ismeuv(promote(float(wave), float(hcol), float(he1col),
                    float(he2col))..., fano)
