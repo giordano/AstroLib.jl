a63e8e0e5991fa20d667cf652053f4722725e740
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2017-06-10 22:50:43 +0530

    Modify code to accomodate for scalar first argument
    Make the first argument a scalar and take advantage of Julia's
    syntactic vectorization with dot call by modifying the code accordingly.
    Code now runs around 7 times faster via benchmarking.

--- src/ismeuv.jl
+++ src/ismeuv.jl
@@ -1,72 +1,47 @@
 # This file is a part of AstroLib.jl. License is MIT "Expat".
 
-function _ismeuv{T<:AbstractFloat}(wave::AbstractVector{T}, hcol::T, he1col::T, he2col::T, fano::Bool)
-    ratio = wave/911.75
-    tauH = zeros(wave)
-    good = find(ratio.< 1)
-    minexp = -708.39642 # from alog((machar(double=double).xmin)) where double = 1b in IDL
-
-    if length(good) != 0
-        r = ratio[good]
-        z = sqrt.((1.0 - r).\r)
-        denom = ones(Float64,length(good))
-        y = -2*π*z
-        good1 = find(y.> minexp)
-
-        if length(good1) != 0
-            denom[good1] = -expm1.(y[good1])
-        end
-        tauH[good] = denom.\(hcol.*(3.44e-16).*(r.^4).*exp.(-4.0.*z.*atan.(z.\1)))
+function _ismeuv{T<:AbstractFloat}(wave::T, hcol::T, he1col::T, he2col::T, fano::Bool)
+    r = wave/911.75
+    if r > 1
+        return T(0)
     end
-    tauHe2 = zeros(wave)
-    ratio *= 4
-    good = find(ratio.< 1)
-
-    if length(good) != 0
-        r = ratio[good]
-        z = sqrt.((1.0 - r).\r)
-        denom = 4*ones(Float64,length(good))
-        y = -2*π*z
-        good1 = find(y.> minexp)
-
-        if length(good1) != 0
-            denom[good1] .*= -expm1.(y[good1])
-        end
-        tauHe2[good] = denom.\(he2col.*(3.44e-16).*(r.^4).*exp.(-4.0.*z.*atan.(z.\1)))
+    # minexp = -708.39642  from alog((machar(double=double).xmin)) where double = 1b in IDL
+    z = sqrt(r/(1.0 - r))
+    denom = -expm1(-2*π*z)
+    tauH = (hcol*(3.44e-16)*(r^4)*exp(-4.0*z*atan(1/z)))/denom
+    r *= 4
+    tauHe2 = zero(T)
+    if r < 1
+        z = sqrt(r/(1.0 - r))
+        denom = T(4)
+        denom *= -expm1(-2*π*z)
+        tauHe2 = (he2col*(3.44e-16)*(r^4)*exp(-4.0*z*atan(1/z)))/denom
     end
     q  = [2.81, 2.51, 2.45, 2.44]
     nu = [1.610, 2.795, 3.817, 4.824]
     fano_gamma = [2.64061e-03, 6.20116e-04, 2.56061e-04, 1.320159e-04]
     esubi = 4.807317 - (nu.^2).\1
-    tauHe1 = zeros(wave)
-    good = find(wave.< 503.97)
-
-    if length(good) != 0
-        x = log10.(wave[good])
-        y = zeros(x)
-        good1 = find(wave.< 46)
-        if length(good1) != 0
-            for i in good1
-                y[i] = @evalpoly x[i] -2.465188e+01 4.354679 -3.553024 5.573040 -5.872938 3.720797 -1.226919 1.576657e-01
-            end
-        end
-        good1 = find(wave[good].> 46)
-
-        if length(good1) != 0
-            for i in good1
-                y[i] = @evalpoly x[i] -2.953607e+01 7.083061 8.678646e-01 -1.221932 4.052997e-02 1.317109e-01 -3.265795e-02 2.500933e-03
-            end
+    tauHe1 = zero(T)
+    if wave < 503.97
+        x = log10(wave)
+        if wave < 46
+            y = @evalpoly(x, -2.465188e+01, 4.354679, -3.553024, 5.573040,
+                          -5.872938, 3.720797, -1.226919, 1.576657e-01)
+        else
+            y = @evalpoly(x, -2.953607e+01, 7.083061, 8.678646e-01, -1.221932,
+                          4.052997e-02, 1.317109e-01, -3.265795e-02,
+                          2.500933e-03)
             if fano
-                episilon = wave.\911.2671
+                episilon = 911.2671/wave
                 for i = 1:4
                     x = 2*(episilon - esubi[i])/fano_gamma[i]
-                    y = y + log10.((1 + x.^2).\(x - q[i]).^2)
+                    y += log10(((x - q[i])^2)/(1 + x^2))
                 end
             end
         end
-    tauHe1[good] = he1col*exp10.(y)
+    tauHe1 = he1col*exp10(y)
     end
-    return tauH + tauHe1+ tauHe2
+    return tauH + tauHe1 + tauHe2
 end
 
 """
@@ -82,8 +57,8 @@ The EUV optical depth is computed from the photoionization of hydrogen and heliu
 
 ### Arguments ###
 
-* `wave`: vector of wavelength values (in Angstroms). Useful range is 40 - 912 A;
-  at shorter wavelengths metal opacity should be considered, at longer wavelengths
+* `wave`: scalar of wavelength value (in Angstroms). Useful range is 40 - 912 A;
+  at shorter wavelength metal opacity should be considered, at longer wavelengths
   there is no photoionization.
 * `hcol`: scalar specifying interstellar hydrogen column density in cm-2.
 * `he1col` (optional): scalar specifying neutral helium column density in cm-2.
@@ -107,7 +82,7 @@ The EUV optical depth is computed from the photoionization of hydrogen and heliu
 One has a model EUV spectrum with wavelength, w (in Angstroms).
 Find the EUV optical depth by 1e18 cm-2 of HI, with N(HeI)/N(HI) = N(HeII)/N(HI) = 0.05.
 
-julia> ismeuv([670, 910], 1e19, 5e17, 5e17)
+julia> ismeuv.([670, 910], 1e19, 5e17, 5e17)
 2-element Array{Float64,1}:
  27.3539
  62.6838
@@ -117,7 +92,7 @@ julia> ismeuv([670, 910], 1e19, 5e17, 5e17)
 
 Code of this function is based on IDL Astronomy User's Library.
 """
-ismeuv(wave::AbstractVector{<:Real}, hcol::Real, he1col::Real=hcol*0.1, he2col::Real=0,
+ismeuv(wave::Real, hcol::Real, he1col::Real=hcol*0.1, he2col::Real=0,
        fano::Bool=false) =
-           _ismeuv(float(wave), promote(float(hcol), float(he1col), float(he2col))...,
-                   fano)
+           _ismeuv(promote(float(wave), float(hcol), float(he1col),
+                   float(he2col))..., fano)
