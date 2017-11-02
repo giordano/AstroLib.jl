6a656d49c3465309b0ebb05fc9501d38b4da2e04
autodocs <autodocs>
2017-11-02 10:26:42 +0000

    build based on 919d6fa

--- latest/search_index.js
+++ latest/search_index.js
@@ -177,7 +177,7 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "ref.html#AstroLib.adstring-Union{Tuple{T}, Tuple{T,T}} where T<:AbstractFloat",
+    "location": "ref.html#AstroLib.adstring-Union{Tuple{T,T}, Tuple{T}} where T<:AbstractFloat",
     "page": "Reference",
     "title": "AstroLib.adstring",
     "category": "Method",
@@ -425,7 +425,7 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "ref.html#AstroLib.imf-Tuple{AbstractArray{#s61,1} where #s61<:Real,AbstractArray{#s60,1} where #s60<:Real,AbstractArray{#s54,1} where #s54<:Real}",
+    "location": "ref.html#AstroLib.imf-Tuple{AbstractArray{#s62,1} where #s62<:Real,AbstractArray{#s61,1} where #s61<:Real,AbstractArray{#s60,1} where #s60<:Real}",
     "page": "Reference",
     "title": "AstroLib.imf",
     "category": "Method",
@@ -621,7 +621,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Reference",
     "title": "AstroLib.premat",
     "category": "Method",
-    "text": "premat(equinox1, equinox2[, FK4=true]) -> precession_matrix\n\nPurpose\n\nReturn the precession matrix needed to go from equinox1 to equinox2.\n\nExplanation\n\nThis matrix is used by precess and baryvel to precess astronomical coordinates.\n\nArguments\n\nequinox1: original equinox of coordinates.\nequinox2: equinox of precessed coordinates.\nFK4 (optional boolean keyword): if this keyword is set to true, the FK4 (B1950.0) system precession angles are used to compute the precession matrix. When it is false, the default, use FK5 (J2000.0) precession angles.\n\nOutput\n\nA 3×3 AbstractFloat matrix, used to precess equatorial rectangular coordinates.\n\nExample\n\nReturn the precession matrix from 1950.0 to 1975.0 in the FK4 system\n\njulia> using AstroLib\n\njulia> premat(1950,1975,FK4=true)\n3×3 Array{Float64,2}:\n 0.999981    -0.00558775  -0.00242909\n 0.00558775   0.999984    -6.78691e-6\n 0.00242909  -6.78633e-6   0.999997\n\nMethod\n\nFK4 constants from \"Computational Spherical Astronomy\" by Taff (1983), p. 24. (FK4). FK5 constants from \"Explanatory Supplement To The Astronomical Almanac\" 1992, page 104 Table 3.211.1 (https://archive.org/details/131123ExplanatorySupplementAstronomicalAlmanac).\n\nNotes\n\nCode of this function is based on IDL Astronomy User's Library.\n\n\n\n"
+    "text": "premat(equinox1, equinox2[, FK4=true]) -> precession_matrix\n\nPurpose\n\nReturn the precession matrix needed to go from equinox1 to equinox2.\n\nExplanation\n\nThis matrix is used by precess and baryvel to precess astronomical coordinates.\n\nArguments\n\nequinox1: original equinox of coordinates.\nequinox2: equinox of precessed coordinates.\nFK4 (optional boolean keyword): if this keyword is set to true, the FK4 (B1950.0) system precession angles are used to compute the precession matrix. When it is false, the default, use FK5 (J2000.0) precession angles.\n\nOutput\n\nA 3×3 matrix, used to precess equatorial rectangular coordinates.\n\nExample\n\nReturn the precession matrix from 1950.0 to 1975.0 in the FK4 system\n\njulia> using AstroLib\n\njulia> premat(1950,1975,FK4=true)\n3×3 StaticArrays.SArray{Tuple{3,3},Float64,2,9}:\n 0.999981    -0.00558775  -0.00242909\n 0.00558775   0.999984    -6.78691e-6\n 0.00242909  -6.78633e-6   0.999997\n\nMethod\n\nFK4 constants from \"Computational Spherical Astronomy\" by Taff (1983), p. 24. (FK4). FK5 constants from \"Explanatory Supplement To The Astronomical Almanac\" 1992, page 104 Table 3.211.1 (https://archive.org/details/131123ExplanatorySupplementAstronomicalAlmanac).\n\nNotes\n\nCode of this function is based on IDL Astronomy User's Library.\n\n\n\n"
 },
 
 {
